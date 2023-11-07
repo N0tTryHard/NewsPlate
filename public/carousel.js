631682b698ee491ccca75969c608e7cb57c6acb9
Andrey Delimarskiy <lekas1988@gmail.com>
2023-11-07 12:48:02 +0300

    what

--- public/carousel.js
+++ public/carousel.js
@@ -1,79 +1,103 @@
 let timeoutId;
+let timeoutId2;
 let pageId = 1;
 const pageIds = ['one', 'two', 'three', 'four', 'five'];
 const idToPage = {'one': 0, 'two': 1, 'three': 2, 'four': 3, 'five': 4};
 
-function tonull(x) {
-    if (x > 4) {
-        x = 0;
-    }
-    return x;
+// function tonull(x) {
+//     if (x > 4) {
+//         x = 0;
+//     }
+//     return x;
+// }
+//
+// function changeClasses(flag=5000) {
+//     const elements = document.querySelectorAll('.imageBar .imageContainer');
+//
+//     elements.forEach(element => {
+//         if (element.classList.contains('one')) {
+//             element.classList.remove('one');
+//             element.classList.add('two');
+//         } else if (element.classList.contains('two')) {
+//             element.classList.remove('two');
+//             element.classList.add('three');
+//         } else if (element.classList.contains('three')) {
+//             element.classList.remove('three');
+//             element.classList.add('four');
+//         } else if (element.classList.contains('four')) {
+//             element.classList.remove('four');
+//             element.classList.add('five');
+//         } else if (element.classList.contains('five')) {
+//             element.classList.remove('five');
+//             element.classList.add('one');
+//         }
+//     });
+//     const points = document.querySelectorAll('.point');
+//     points.forEach(point => {
+//         point.classList.remove('active');
+//     });
+//     const currentPoint = document.querySelector('.point.' + pageIds[pageId]);
+//     if (currentPoint) {
+//         currentPoint.classList.add('active');
+//     }
+//
+//     pageId = tonull(pageId + 1);
+//     timeoutId = setTimeout(changeClasses, flag);
+// }
+//
+// timeoutId = setTimeout(changeClasses, 5000);
+//
+// const elements = document.querySelectorAll('.imageBar .imageContainer');
+// elements.forEach(element => {
+//     element.addEventListener('mouseenter', () => {
+//         if (element.classList.contains('three')) {
+//             clearTimeout(timeoutId);
+//         }
+//     });
+//
+//     element.addEventListener('mouseleave', () => {
+//         if (element.classList.contains('three')) {
+//             timeoutId = setTimeout(changeClasses, 5000);
+//         }
+//     });
+// });
+
+// const pointes = document.querySelectorAll('.points .point');
+// function handleClick() {
+//     const target = event.target;
+//     const classes = target.classList[1];
+//     let toPage = idToPage[classes];
+//
+//     while (pageId !== toPage) {
+//         console.log(pageId, toPage);
+//         clearTimeout(timeoutId);
+//         changeClasses(10)
+//         pageId = tonull(pageId + 1);
+//     }
+// }
+// pointes.forEach(point => {
+//     point.addEventListener('click', handleClick);
+// });
+
+const option = document.querySelectorAll('.option');
+function clicker() {
+    option.forEach(element =>{
+        element.classList.remove('active')
+    })
+    this.classList.add('active')
+}
+option.forEach(opt => {
+    opt.addEventListener('click', clicker);
+})
+
+function turner() {
+    option.forEach(element =>{
+        element.classList.remove('active')
+    })
+    option[pageId].classList.add('active')
+    console.log(pageId)
+    pageId = (pageId + 1) % 5
+    timeoutId2 = setTimeout(turner, 5000);
 }
 
-function changeClasses(flag=5000) {
-    const elements = document.querySelectorAll('.imageBar .imageContainer');
-    
-    elements.forEach(element => {
-        if (element.classList.contains('one')) {
-            element.classList.remove('one');
-            element.classList.add('two');
-        } else if (element.classList.contains('two')) {
-            element.classList.remove('two');
-            element.classList.add('three');
-        } else if (element.classList.contains('three')) {
-            element.classList.remove('three');
-            element.classList.add('four');
-        } else if (element.classList.contains('four')) {
-            element.classList.remove('four');
-            element.classList.add('five');
-        } else if (element.classList.contains('five')) {
-            element.classList.remove('five');
-            element.classList.add('one');
-        }
-    });
-    const points = document.querySelectorAll('.point');
-    points.forEach(point => {
-        point.classList.remove('active');
-    });
-    const currentPoint = document.querySelector('.point.' + pageIds[pageId]);
-    if (currentPoint) {
-        currentPoint.classList.add('active');
-    }
-    
-    pageId = tonull(pageId + 1);
-    timeoutId = setTimeout(changeClasses, flag);
-}
-
-timeoutId = setTimeout(changeClasses, 5000);
-
-const elements = document.querySelectorAll('.imageBar .imageContainer');
-elements.forEach(element => {
-    element.addEventListener('mouseenter', () => {
-        if (element.classList.contains('three')) {
-            clearTimeout(timeoutId);
-        }
-    });
-    
-    element.addEventListener('mouseleave', () => {
-        if (element.classList.contains('three')) {
-            timeoutId = setTimeout(changeClasses, 5000);
-        }
-    });
-});
-
-const pointes = document.querySelectorAll('.points .point');
-function handleClick() {
-    const target = event.target;
-    const classes = target.classList[1];
-    let toPage = idToPage[classes];
-    
-    while (pageId !== toPage) {
-        console.log(pageId, toPage);
-        clearTimeout(timeoutId);
-        changeClasses(10)
-        pageId = tonull(pageId + 1);
-    }
-}
-pointes.forEach(point => {
-    point.addEventListener('click', handleClick);
-});
+timeoutId2 = setTimeout(turner, 5000);
--- public/style.js
+++ public/style.js
@@ -20,55 +20,55 @@ window.addEventListener('load', updateStyles);
 window.addEventListener('scroll', updateStyles);
 
 
-document.addEventListener("DOMContentLoaded", function() {
-    const imageContainerLeft = document.getElementById('imageContainerLeft');
-    const imageContainerRight = document.getElementById('imageContainerRight');
-    images = []; // Populate this array with your images from assets folder
-
-    for (i=1;i<12;i++){
-        images.push(`img${i}.jpg`);
-    };
-    console.log(images);
-    // Append images to the left container
-    images.forEach(imgSrc => {
-        const imgElem = createImageElement(imgSrc);
-        imageContainerLeft.appendChild(imgElem);
-    });
-
-    // Append images to the right container
-    images.forEach(imgSrc => {
-        const imgElem = createImageElement(imgSrc);
-        imageContainerRight.appendChild(imgElem);
-    });
-
-    function createImageElement(imgSrc) {
-        const imgElem = document.createElement("img");
-        imgElem.src = "assets/" + imgSrc;
-        imgElem.onclick = function() {
-            window.open(imgElem.src); // Open image in new tab when clicked
-        }
-        return imgElem;
-    }
-
-    function scrollImages() {
-        scrollImagesForContainer(imageContainerLeft, 1);
-        scrollImagesForContainer(imageContainerRight, -1);
-        requestAnimationFrame(scrollImages);
-    }
-
-    function scrollImagesForContainer(container, direction) {
-        const imgs = container.querySelectorAll('img');
-        imgs.forEach(img => {
-            const bottomPosition = parseInt(window.getComputedStyle(img).bottom);
-            if (direction === 1 && bottomPosition >= container.clientHeight) {
-                img.style.bottom = (-img.clientHeight) + 'px';
-            } else if (direction === -1 && bottomPosition <= -img.clientHeight) {
-                img.style.bottom = container.clientHeight + 'px';
-            } else {
-                img.style.bottom = (bottomPosition + direction) + 'px'; 
-            }
-        });
-    }
-
-    requestAnimationFrame(scrollImages);
-});
+// document.addEventListener("DOMContentLoaded", function() {
+//     const imageContainerLeft = document.getElementById('imageContainerLeft');
+//     const imageContainerRight = document.getElementById('imageContainerRight');
+//     images = []; // Populate this array with your images from assets folder
+//
+//     for (i=1;i<12;i++){
+//         images.push(`img${i}.jpg`);
+//     };
+//     console.log(images);
+//     // Append images to the left container
+//     images.forEach(imgSrc => {
+//         const imgElem = createImageElement(imgSrc);
+//         imageContainerLeft.appendChild(imgElem);
+//     });
+//
+//     // Append images to the right container
+//     images.forEach(imgSrc => {
+//         const imgElem = createImageElement(imgSrc);
+//         imageContainerRight.appendChild(imgElem);
+//     });
+//
+//     function createImageElement(imgSrc) {
+//         const imgElem = document.createElement("img");
+//         imgElem.src = "assets/" + imgSrc;
+//         imgElem.onclick = function() {
+//             window.open(imgElem.src); // Open image in new tab when clicked
+//         }
+//         return imgElem;
+//     }
+//
+//     function scrollImages() {
+//         scrollImagesForContainer(imageContainerLeft, 1);
+//         scrollImagesForContainer(imageContainerRight, -1);
+//         requestAnimationFrame(scrollImages);
+//     }
+//
+//     function scrollImagesForContainer(container, direction) {
+//         const imgs = container.querySelectorAll('img');
+//         imgs.forEach(img => {
+//             const bottomPosition = parseInt(window.getComputedStyle(img).bottom);
+//             if (direction === 1 && bottomPosition >= container.clientHeight) {
+//                 img.style.bottom = (-img.clientHeight) + 'px';
+//             } else if (direction === -1 && bottomPosition <= -img.clientHeight) {
+//                 img.style.bottom = container.clientHeight + 'px';
+//             } else {
+//                 img.style.bottom = (bottomPosition + direction) + 'px';
+//             }
+//         });
+//     }
+//
+//     requestAnimationFrame(scrollImages);
+// });
